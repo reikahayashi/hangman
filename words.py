3e0a1daee5bff6f97e83f997416c55929102791b
Reika H <61173448+reikahayashi@users.noreply.github.com>
2021-02-07 13:44:19 -0500

    Add files via upload

--- words.py
+++ words.py
@@ -1,1 +1,11 @@
-words = ["hi","coding","how","are","you","fun","python","books","phone","tech","nice","lovely","water","food","shelter","earth","soil","sea","mountain","gorgeous","youtube","music","sounds","taste","hear","see","touch"]
+
+
+words = [
+'hi','bye','hey','say','coding','fun','jump','air','fly',
+'helicopter','airplane','birds','sky','wind','earth','soil','plants',
+'grass','flowers','trees','nature','hike','ice skate','skiiing','snow','christmas',
+'joy','music','singing','dancing','arts','match','climb','lying','emerald',
+'magic', 'exercise','movies'
+
+
+]
